c600e15507f4c806d94fff0b82dad3023d798518
Eamonn Mansour <47121388+eamansour@users.noreply.github.com>
2026-06-11 13:42:51 +0100

    automation: Add workflow to add newly released CLI to scoop bucket
    Signed-off-by: Eamonn Mansour <47121388+eamansour@users.noreply.github.com>

--- add-version.sh
+++ add-version.sh
@@ -19,13 +19,13 @@ BUCKET_DIR="${SCRIPT_DIR}/bucket"
 #-----------------------------------------------------------------------------------------
 # Set Colors
 #-----------------------------------------------------------------------------------------
-bold=$(tput bold)
-underline=$(tput sgr 0 1)
-reset=$(tput sgr0)
-red=$(tput setaf 1)
-green=$(tput setaf 76)
-white=$(tput setaf 7)
-blue=$(tput setaf 25)
+bold="\033[1m"
+underline="\033[4m"
+reset="\033[0m"
+red="\033[31m"
+green="\033[32m"
+white="\033[37m"
+blue="\033[34m"
 
 #-----------------------------------------------------------------------------------------
 # Headers and Logging
@@ -80,7 +80,7 @@ function create_versioned_manifest() {
   "license": "EPL-2.0",
   "architecture": {
     "64bit": {
-      "url": "https://github.com/galasa-dev/galasa/releases/download/v$version_to_add/galasactl-windows-x86_64.exe",
+      "url": "https://github.com/eamansour/galasa/releases/download/v$version_to_add/galasactl-windows-x86_64.exe",
       "hash": "$hash",
       "bin": [
         [
@@ -91,12 +91,12 @@ function create_versioned_manifest() {
     }
   },
   "checkver": {
-    "github": "https://github.com/galasa-dev/galasa"
+    "github": "https://github.com/eamansour/galasa"
   },
   "autoupdate": {
     "architecture": {
       "64bit": {
-        "url": "https://github.com/galasa-dev/galasa/releases/download/v\$version/galasactl-windows-x86_64.exe"
+        "url": "https://github.com/eamansour/galasa/releases/download/v\$version/galasactl-windows-x86_64.exe"
       }
     }
   }
@@ -120,7 +120,7 @@ function update_latest_manifest() {
   "license": "EPL-2.0",
   "architecture": {
     "64bit": {
-      "url": "https://github.com/galasa-dev/galasa/releases/download/v$version_to_add/galasactl-windows-x86_64.exe",
+      "url": "https://github.com/eamansour/galasa/releases/download/v$version_to_add/galasactl-windows-x86_64.exe",
       "hash": "$hash",
       "bin": [
         [
@@ -131,12 +131,12 @@ function update_latest_manifest() {
     }
   },
   "checkver": {
-    "github": "https://github.com/galasa-dev/galasa"
+    "github": "https://github.com/eamansour/galasa"
   },
   "autoupdate": {
     "architecture": {
       "64bit": {
-        "url": "https://github.com/galasa-dev/galasa/releases/download/v\$version/galasactl-windows-x86_64.exe"
+        "url": "https://github.com/eamansour/galasa/releases/download/v\$version/galasactl-windows-x86_64.exe"
       }
     }
   }
@@ -179,7 +179,7 @@ mkdir -p "${SCRIPT_DIR}/temp"
 check_exit_code $? "Couldn't create temporary folder"
 
 # Download the Windows binary from GitHub releases
-url_to_download_from="https://github.com/galasa-dev/galasa/releases/download/v${version_to_add}/galasactl-windows-x86_64.exe"
+url_to_download_from="https://github.com/eamansour/galasa/releases/download/v${version_to_add}/galasactl-windows-x86_64.exe"
 target_file_path="${SCRIPT_DIR}/temp/galasactl-windows-x86_64.exe"
 
 download_executable "$url_to_download_from" "$target_file_path"
